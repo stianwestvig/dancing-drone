86e6aaa5330dbca893792cef65cf1beaec8384c3
lalaina.razafindraibe <lalaina.razafindraibe@bouvet.no>
2018-02-10 15:13:45 +0100

    Add more moves for left, right and flip

--- moves.js
+++ moves.js
@@ -1,3 +1,49 @@
+function takeOff(drone) {
+    return {
+        delay: 1000,
+        task: () => {
+            drone.takeOff();
+            drone.flatTrim();
+        }
+    }
+}
+
+function landing(drone) {
+    return {
+        delay: 1000,
+        task: () => drone.land()
+    }
+}
+
+function moveLeft(drone) {
+    return {
+        delay: 2000,
+        task: () => {
+            drone.left({steps: 5});
+            drone.flatTrim();
+        }
+    }
+}
+
+function moveRight(drone) {
+    return {
+        delay: 2000,
+        task: () => {
+            drone.right({steps: 5});
+            drone.flatTrim();
+        }
+    }
+}
+
+function frontFlip(drone) {
+    return {
+        delay: 5000,
+        task: () => {
+            drone.frontFlip({steps: 5});
+            drone.flatTrim();
+        }
+    }
+};
 
 function upAndDown(drone) {
     return [
@@ -15,11 +61,41 @@ function upAndDown(drone) {
     ]
 }
 
+function leftToRight(drone) {
+    return [
+        {
+            delay: 1000,
+            task: () => {
+                drone.takeOff();
+                drone.flatTrim();
+            }
+        },
+        {
+            delay: 2000,
+            task: () => {
+                drone.left({steps: 5});
+                drone.flatTrim();
+            }
+        },
+        {
+            delay: 2000,
+            task: () => {
+                drone.right({steps: 5});
+                drone.flatTrim();
+            }
+        },
+        {
+            delay: 1000,
+            task: () => drone.land()
+        }
+    ]
+}
+
 
 function getMoves (drone) {
     return [{
         meta: {},
-        instructions: upAndDown(drone)
+        instructions: [takeOff(drone), moveLeft(drone), moveRight(drone), frontFlip(drone), landing(drone)]
     }];
 }
 
