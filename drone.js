12493821fc0d68971e45e4521333337961c2e8d6
Stian Westvig <stian.westvig@bouvet.no>
2018-02-10 11:22:57 +0100

    testing the drone

--- drone.js
+++ drone.js
@@ -5,3 +5,47 @@
 // todo: connect to drone
 
 // todo: let brain choose move based on music
+
+
+// drone 1: Mambo_612553
+
+const RollingSpider = require('rolling-spider');
+const temporal = require('temporal');
+const rollingSpider = new RollingSpider();
+
+rollingSpider.connect(() => {
+
+
+  rollingSpider.setup(() => {
+    rollingSpider.flatTrim();
+    rollingSpider.startPing();
+    rollingSpider.flatTrim();
+
+    console.log('Connected to drone', rollingSpider.name);
+
+    temporal.queue([
+      {
+        delay: 1000,
+        task: () => {
+          rollingSpider.takeOff();
+          rollingSpider.flatTrim();
+        }
+      },
+      {
+        delay: 1000,
+        task: () => rollingSpider.forward({steps: 12})
+      },
+      {
+        delay: 1000,
+        task: () => rollingSpider.land()
+      },
+      {
+        delay: 1000,
+        task: () => {
+          temporal.clear();
+          process.exit(0);
+        }
+      }
+    ]);
+  });
+});
